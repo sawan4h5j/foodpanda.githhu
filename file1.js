b19d192e5ca08775756e6af27107e39f155458ea
sawan4h5j <154305330+sawan4h5j@users.noreply.github.com>
2023-12-20 19:03:25 +0600

    Add files via upload

---
+++ file1.js
@@ -0,0 +1,57 @@
+// console.log ('Hello world')
+
+// var Friend ='foridul islam';
+// console.log(Friend )
+
+
+// var styType = "sawan programmar";
+
+// console.log(typeof styType)
+
+// var numaType = 76237356473;
+
+console.log (typeof numaType)
+
+var Booltype = true;
+
+// console.log(typeof Booltype)
+
+var rochona="lorem 6y46ryu45 457y4 565 456  564 "
+
+// console.log(rochona.toUpperCase())
+// console.log(rochona.tolowerCase())
+
+// var myfnd = "Forid Mohin Tasin Siam Sawan"
+
+// console.log(my Friend.split(forid));
+
+// console.log(myfnd.indexOf("M"))
+
+// var num1 = "55";
+
+// var num2 = 99;
+
+// var mot = num1 + num2;
+// console.log(mot);
+//  var num3 = 20;
+//  var num4 = 90;
+
+//  num4 = "" + num4;
+//  var total = num3 + num4;
+
+//  console.log(total)
+
+//  var num1 = 60;
+//  var num2 = 70;
+
+//  console.log(num1 * num2)
+
+//  var num1 = -20;
+
+//  var sum = Math.abs(num1)
+//  console.log(sum)
+
+//  var voter = 0.99999;
+
+//  var sum = math.round/cli/floor/random(voter)
+//  console.log(sum)
